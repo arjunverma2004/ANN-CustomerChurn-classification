ac54271d61f22811931b117c1fe5b8855fd7662c
arjunverma2004 <163740749+arjunverma2004@users.noreply.github.com>
2025-07-22 13:47:36 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -76,16 +76,13 @@ geo_encoded_df = pd.DataFrame(geo_encoded, columns=onehot_encoder_geo.get_featur
 input_data = pd.concat([input_data.reset_index(drop=True), geo_encoded_df], axis=1)
 
 # Scale the input data
-input_data_scaled = scaler.transform(input_data)
-
-
-
-
 
 
 
 if st.button("Predict"):
     try:
+        # Scale the input data
+        input_data_scaled = scaler.transform(input_data)
         # Predict churn
         prediction = model.predict(input_data_scaled)
         prediction_proba = prediction[0][0]
